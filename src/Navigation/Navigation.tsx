3ad60195cc55928019f4c46a0a59a43e59f77dee
Loan Alouache <alouache.loan@gmail.com>
2021-03-01 11:37:33 +0100

    Switched register/login screen order

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -14,8 +14,8 @@ export default class Navigation extends React.Component {
     render() {
         return (
             <Stack.Navigator>
-                <Stack.Screen name="Register" component={Register} />
                 <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
                 <Stack.Screen name="Map" component={Map} />
                 <Stack.Screen name="Challenge">{(props) => <Challenge {...props}></Challenge>}</Stack.Screen>
                 <Stack.Screen name="Overlay" component={Overlay} />
